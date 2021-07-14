82edb9a293ea2693c4a0cf2de35495ada9f426db
matthew morrish <86901411+matthewmorrish@users.noreply.github.com>
2021-07-14 14:09:10 -0700

    Updated from live response generation to pre-test response generation
    Opted to move from the format of the client making an n-xiB request and the host generating that data on the spot then replying with it.  Moved to the client sending an initial request then having the host generate this data upfront and replying with it for each following request.  This should more accurately replicate the conditions of a database already having data in-memory and the client making a request for data.
    
    Also changed some comments to be more descriptive, moved functions around, cut out some fat.
    
    Client:
        - Changed client request-message from reusable to a list of requests
        - Added an initial request that tells the host what data to generate upfront
        - Added handling for likely errors
    
    Host:
        - Added a memory-check to see if host is capable of supporting the clients demands
        - Changed some one-line functions to lambdas
        - Added some pretty user updates
        - Added handling for initial request/proceeding requests

--- network_test_client.py
+++ network_test_client.py
@@ -8,7 +8,8 @@ Matthew Morrish
 
 
 # Imports
-import socket, time
+import socket
+from time import perf_counter
 
 
 # Set vars
@@ -16,13 +17,13 @@ host        = '192.168.1.234'
 port        = 5005
 request_len = 32
 
-unit       = 'mib'
+unit       = 'mib' # !!! NEED A LIMITER THAT GOES UP TO THE PORTS MAXIMUM BUFFER SIZE
 multiplier = 100
 cycles     = 10
 response_len = 1024 ** {'b': 0, 'kib': 1, 'mib': 2, 'gib': 3}[unit] * multiplier
 
 
-# Transfers 'msg' to host while timing cycle
+# Make request to host while timing cycle
 def transfer(msg):
     try:
         # Build socket & connect to host
@@ -30,16 +31,16 @@ def transfer(msg):
         s.connect((host, port))
 
         # Start timer & send
-        core_start = time.perf_counter()
+        core_start = perf_counter()
         s.sendall(msg)
 
         # Receive & end timer
         response = s.recv(response_len)
-        core_time = time.perf_counter()-core_start
+        core_time = perf_counter()-core_start
 
-        # Close socket & return core-time, size of data received (in bytes)
+        # Close socket & return core-time, data received
         s.close()
-        return core_time, len(response)
+        return core_time, response
 
     # In-case our socket fails
     except Exception as e:
@@ -48,7 +49,6 @@ def transfer(msg):
 
 # Convert machine-readable to human-readable
 def toHumanReadable(num):
-
     for unit in ['B', 'KiB', 'MiB']:
         if num < 1024.0:
             return f'{round(num, 3)} {unit}'
@@ -57,49 +57,67 @@ def toHumanReadable(num):
 
 
 def main():
-
     # Make things more legible
     space_format = lambda curr, tot : ' ' * (len(str(tot)) - len(str(curr)) + 1)
     time_format  = lambda time : (str(time) + '0' * (7 - len(str(time))))[0:6]
 
-    # Prep & pad message (32-byte padding is 3-byte offset to account for b'')
-    msg = ';' + str(multiplier) + ':' + str(unit)
-    msg = msg.zfill(request_len-3)
-    msg = msg.encode()
+    # Prep & pad request list (32-byte padding is 3-byte offset to account for b'')
+    msg_li = [(';' + str(i) + ':' + str(cycles) + ':' + str(multiplier) + ':' + str(unit)).zfill(request_len-3).encode() for i in range(cycles+1)]
 
     # Update user
-    print(f'\nRequesting [{toHumanReadable(response_len)}] from [{host}:{port}] with [{request_len} Bytes] of data:')
-    
-    # Run loop
-    core_total, core_rate_total = 0, 0
-    core_time_total, received_total, failed, core_rate_li = 0, 0, 0, []
-    for i in range(1, cycles+1):
+    print(f'\nRequesting [{toHumanReadable(response_len)}] from [{host}:{port}] with [{request_len} Bytes] of data: ', end='')
 
-        run_results = transfer(msg)
+    # Make initial request
+    init_results = transfer(msg_li.pop(0))
 
-        if isinstance(run_results, Exception):
-            failed +=1
-            core_rate_li.append(0) # Patch so our final min/max have something to work with
+    # Handle for socket exceptions
+    if isinstance(init_results, Exception):
+        print(f'[FAILED]\n  {init_results}')
 
-            print(f'  Test{space_format(i, cycles)}[{i}/{cycles}]: Failed... {run_results}')
+    else:
+        # Parse results
+        init_results = init_results[1].decode('utf-8').strip("'").split(';')[1]
 
-        else:
-            core_time_total += run_results[0]
-            received_total  += run_results[1]
-            core_rate        = run_results[1] / run_results[0]
-            core_rate_li.append(core_rate)
+        # Handle for host-side insufficient memory
+        if init_results == 'self_test_fail':
+            print('[FAILED]\n  Host self-test reported insufficient memory...')
 
-            print(f'  Test{space_format(i, cycles)}[{i}/{cycles}]:',
-                  f'Time: [{time_format(run_results[0])}(s)], Bytes Received: [{toHumanReadable(run_results[1])}]')
+        # Proceed with test
+        elif init_results == 'self_test_pass':
+            print('[PASS]')
 
-    # Update user on final results
-    print(f'\nResponse statistics for [{host}]:\n',
-          f'  Received: [{cycles-failed}],',
-          f'Lost: [{failed} - {round((1-received_total/(response_len*cycles))*100, 2)}% loss]',
-          f'\nAverage response rate ([{request_len} Byte] request inclusive):\n',
-          f'  Minimum: [{toHumanReadable(min(core_rate_li))}/(s)],',
-          f'Maximum: [{toHumanReadable(max(core_rate_li))}/(s)],',
-          f'Average: [{toHumanReadable(sum(core_rate_li) / len(core_rate_li))}/(s)]')
+            # Run loop
+            core_time_total, received_total, failed, core_rate_li = 0, 0, 0, []
+            for i in range(1, cycles+1):
+
+                # Make request
+                run_results = transfer(msg_li.pop(0))
+
+                # Handle socket exceptions, update user
+                if isinstance(run_results, Exception):
+                    failed += 1
+                    core_rate_li.append(0) # Patch so our final min/max always have something to work with
+
+                    print(f'  Test{space_format(i, cycles)}[{i}/{cycles}]: Failed... {run_results}')
+
+                else:
+                    # Store results, update user
+                    core_time_total += run_results[0]
+                    received_total  += len(run_results[1])
+                    core_rate        = len(run_results[1]) / run_results[0]
+                    core_rate_li.append(core_rate)
+
+                    print(f'  Test{space_format(i, cycles)}[{i}/{cycles}]:',
+                          f'Time: [{time_format(run_results[0])}(s)], Bytes Received: [{toHumanReadable(len(run_results[1]))}]')
+
+            # Update user on final results
+            print(f'\nResponse statistics for [{host}]:\n',
+                  f'  Received: [{cycles-failed}],',
+                  f'Lost: [{failed} - {round((1-received_total/(response_len*cycles))*100, 2)}% loss]',
+                  f'\nAverage response rate ([{request_len} Byte] request inclusive):\n',
+                  f'  Minimum: [{toHumanReadable(min(core_rate_li))}/(s)],',
+                  f'Maximum: [{toHumanReadable(max(core_rate_li))}/(s)],',
+                  f'Average: [{toHumanReadable(sum(core_rate_li) / len(core_rate_li))}/(s)]\n')
 
 
 if __name__ == '__main__':
--- network_test_host.py
+++ network_test_host.py
@@ -8,42 +8,85 @@ Matthew Morrish
 
 
 # Imports
-import socket, os
+import socket
+from os import urandom
+from subprocess import check_output
 
 
 # Set vars
-host   = '127.0.0.1'
-port   = 5005
-buffer = 32
+host       = '192.168.1.234'
+port       = 5005
+buffer     = 32
+mem_buffer = 0 # Percent
 
 
-# Parse received packet & return a byte-literal of requested size
-def genReturnPacket(data):
-    data = data.decode('utf-8').strip("'").split(';')[1].split(':')
-    size = 1024 ** {'b': 0, 'kib': 1, 'mib': 2, 'gib': 3}[data[1]] * int(data[0])
+# Set global var for transfer data
+response_li = []
 
-    print(f'Received request for: {data[0]}{data[1]}')
-    return os.urandom(size)
+
+# Quality-of-life
+getSize   = lambda unit, multiplier : 1024 ** {'b': 0, 'kib': 1, 'mib': 2, 'gib': 3}[unit] * int(multiplier)
+parseData = lambda data : data.decode('utf-8').strip("'").split(';')[1].split(':')
+
+
+# Check to see if the system has sufficient memory (Windows)
+def checkMemWin(unit, multiplier, cycles):
+    req = getSize(unit, multiplier) * int(cycles)
+
+    avail_mem = check_output('wmic OS get FreePhysicalMemory').decode('utf-8')
+    avail_mem = ''.join(ch for ch in avail_mem if ch.isdigit()) # In KiB
+    avail_mem = int(avail_mem) * 1024 # Convert to B
+
+    if avail_mem > req + req*(mem_buffer/100):
+        return True
 
 
 # Main loop
 def main():
+    global response_li
+
     while 1:
-        # Build socket
+        # Build socket & wait
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         s.bind((host, port))
         s.listen(1)
 
         try:
-            # Connect to client, collect packet, reply
-            conn, _ = s.accept()
-            data = conn.recv(buffer)
-            conn.sendall(genReturnPacket(data))
+            # Connect to client, collect & parse request packet
+            conn, addr = s.accept()
+            data = parseData(conn.recv(buffer))
+
+            # Handle for init request, update user
+            if data[0] == '0':
+                print(f'\nHost: Received new test request from [{addr[0]}] for [{data[1]}] cycles of [{data[2]} {data[3]}]')
+
+                # Empty possible remainders in-case a test was interrupted, update user
+                response_li = []
+                print('Host: Memory self-test ', end='')
+
+                # Self-test memory capacity
+                if checkMemWin(data[3], data[2], data[1]):
+                    print('[PASSED]\n...')
+
+                    # Generate a list of byte-literals of requested size & reply sys status good
+                    response_li = [urandom(getSize(data[3], data[2])) for _ in range(int(data[1]))]
+                    conn.sendall(b'00000000000000;self_test_pass')
+
+                # Reply sys status bad
+                else:
+                    print('[FAILED]')
+                    conn.sendall(b'00000000000000;self_test_fail')
+
+            # If not initial request, send test data
+            else:
+                print(f'Host: Replying to request [{data[0]}/{data[1]}]')
+                conn.sendall(response_li.pop())
+                print(f'      [{len(response_li)}] items remaining in memory...')
 
         # In-case our socket fails
         except Exception as e:
-            # print(type(e), e)
+            #print(type(e), e)
             conn.close()
 
 
