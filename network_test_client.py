b07c8df7fa0d3298d2c27346eae9063d1e3a5de3
matthew morrish <86901411+matthewmorrish@users.noreply.github.com>
2021-08-15 13:01:19 -0700

    Added exec args & Linux compatibility
    Client:
        - Added arguments for IP, port, unit, multiplier & number of cycles
        - Added a port max buffer size, set to 10 GiB
    
    Host:
        - Added arguments for IP, port, memory buffer, continuous run & verbosity
        - Added Linux compatibility on memory check
        - Added better error handling
        - Minor tweaks to output dialogue

--- network_test_client.py
+++ network_test_client.py
@@ -1,6 +1,14 @@
+#!/usr/bin/env python3
+
 """
 
-Test network transmission rate - client
+Client
+
+Test local network transmission rate for non-traditional python-specific
+databases that utilize the TCP protocol of the socket library.
+
+Usage:
+    python3 network_test_client.py -ip [HOSTIP] [optional flags] ...
 
 Matthew Morrish
 
@@ -8,19 +16,49 @@ Matthew Morrish
 
 
 # Imports
-import socket
+import argparse, socket
 from time import perf_counter
 
 
 # Set vars
-host        = '192.168.1.234'
-port        = 5005
-request_len = 32
+request_len  = 32 # Don't touch, must match host buffer
 
-unit       = 'mib' # !!! NEED A LIMITER THAT GOES UP TO THE PORTS MAXIMUM BUFFER SIZE
-multiplier = 100
-cycles     = 10
-response_len = 1024 ** {'b': 0, 'kib': 1, 'mib': 2, 'gib': 3}[unit] * multiplier
+
+# Get args
+parser = argparse.ArgumentParser()
+parser.add_argument(
+    '-ip', '--hostip', help='Host machines IP address', 
+    type=str, required=True)
+parser.add_argument(
+    '-p',  '--port', help='Port, default is 5005', 
+    type=int, nargs='?', const=1, default=5005)
+parser.add_argument(
+    '-u',  '--unit', help='Prefix number of bytes to be requested, default is MiB', 
+    type=str, nargs='?', const=1, default='mib')
+parser.add_argument(
+    '-m',  '--multiplier', help='Bytesize multiplier, default is 10', 
+    type=int, nargs='?', const=1, default=10)
+parser.add_argument(
+    '-c', '--cycles', help='Number of cycles to be run, default is 10', 
+    type=int, nargs='?', const=1, default=10)
+args = parser.parse_args()
+
+
+# Check for invalid args
+if args.unit.lower() not in ['b', 'kib', 'mib', 'gib']:
+    raise Exception(f"Invalid unit type '{args.unit}', please use B, KiB, MiB or GiB...")
+else:
+    args.unit = args.unit.lower()
+
+if args.port <= 0 or args.multiplier <= 0 or args.cycles <= 0:
+    raise Exception("Invalid argument, please use positive integers only...")
+
+
+# Get byte literal, check max size
+response_len = 1024 ** {'b': 0, 'kib': 1, 'mib': 2, 'gib': 3}[args.unit] * args.multiplier
+
+if response_len > 10737418240:
+    raise Exception('Requested packet size exceeds maximum buffer size, please request no more than 10GiB/cycle...')
 
 
 # Make request to host while timing cycle
@@ -28,7 +66,7 @@ def transfer(msg):
     try:
         # Build socket & connect to host
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        s.connect((host, port))
+        s.connect((args.hostip, args.port))
 
         # Start timer & send
         core_start = perf_counter()
@@ -62,10 +100,10 @@ def main():
     time_format  = lambda time : (str(time) + '0' * (7 - len(str(time))))[0:6]
 
     # Prep & pad request list (32-byte padding is 3-byte offset to account for b'')
-    msg_li = [(';' + str(i) + ':' + str(cycles) + ':' + str(multiplier) + ':' + str(unit)).zfill(request_len-3).encode() for i in range(cycles+1)]
+    msg_li = [(';' + str(i) + ':' + str(args.cycles) + ':' + str(args.multiplier) + ':' + str(args.unit)).zfill(request_len-3).encode() for i in range(args.cycles+1)]
 
     # Update user
-    print(f'\nRequesting [{toHumanReadable(response_len)}] from [{host}:{port}] with [{request_len} Bytes] of data: ', end='')
+    print(f'\nRequesting [{toHumanReadable(response_len)}] from [{args.hostip}:{args.port}] with [{request_len} Bytes] of data: ', end='')
 
     # Make initial request
     init_results = transfer(msg_li.pop(0))
@@ -88,7 +126,7 @@ def main():
 
             # Run loop
             core_time_total, received_total, failed, core_rate_li = 0, 0, 0, []
-            for i in range(1, cycles+1):
+            for i in range(1, args.cycles+1):
 
                 # Make request
                 run_results = transfer(msg_li.pop(0))
@@ -98,7 +136,7 @@ def main():
                     failed += 1
                     core_rate_li.append(0) # Patch so our final min/max always have something to work with
 
-                    print(f'  Test{space_format(i, cycles)}[{i}/{cycles}]: Failed... {run_results}')
+                    print(f'  Test{space_format(i, args.cycles)}[{i}/{args.cycles}]: Failed... {run_results}')
 
                 else:
                     # Store results, update user
@@ -107,13 +145,13 @@ def main():
                     core_rate        = len(run_results[1]) / run_results[0]
                     core_rate_li.append(core_rate)
 
-                    print(f'  Test{space_format(i, cycles)}[{i}/{cycles}]:',
+                    print(f'  Test{space_format(i, args.cycles)}[{i}/{args.cycles}]:',
                           f'Time: [{time_format(run_results[0])}(s)], Bytes Received: [{toHumanReadable(len(run_results[1]))}]')
 
             # Update user on final results
-            print(f'\nResponse statistics for [{host}]:\n',
-                  f'  Received: [{cycles-failed}],',
-                  f'Lost: [{failed} - {round((1-received_total/(response_len*cycles))*100, 2)}% loss]',
+            print(f'\nResponse statistics for [{args.hostip}]:\n',
+                  f'  Received: [{args.cycles-failed}],',
+                  f'Lost: [{failed} - {round((1-received_total/(response_len*args.cycles))*100, 2)}% loss]',
                   f'\nAverage response rate ([{request_len} Byte] request inclusive):\n',
                   f'  Minimum: [{toHumanReadable(min(core_rate_li))}/(s)],',
                   f'Maximum: [{toHumanReadable(max(core_rate_li))}/(s)],',
--- network_test_host.py
+++ network_test_host.py
@@ -1,6 +1,14 @@
+#!/usr/bin/env python3
+
 """
 
-Test network transmission rate - host
+Host
+
+Test local network transmission rate for non-traditional python-specific
+databases that utilize the TCP protocol of the socket library.
+
+Usage:
+    python3 network_test_host.py [optional flags] ...
 
 Matthew Morrish
 
@@ -8,16 +16,34 @@ Matthew Morrish
 
 
 # Imports
-import socket
-from os import urandom
+import argparse, socket
+from sys import platform
+from os import urandom, popen
 from subprocess import check_output
 
 
 # Set vars
-host       = '192.168.1.234'
-port       = 5005
-buffer     = 32
-mem_buffer = 0 # Percent
+buffer = 32 # Don't touch, must match client request len
+
+
+# Get args
+parser = argparse.ArgumentParser()
+parser.add_argument(
+    '-ip',  '--hostip', help='Manually set host machines IP address, default is automatically detected', 
+    type=str, nargs='?', const=1, default=socket.gethostbyname(socket.gethostname()))
+parser.add_argument(
+    '-p',  '--port', help='Port, default is 5005', 
+    type=int, nargs='?', const=1, default=5005)
+parser.add_argument(
+    '-mb',  '--membuffer', help='Percent of memory to be left unused during peak memory usage, default is 10', 
+    type=int, nargs='?', const=1, default=10)
+parser.add_argument(
+    '-c',  '--continuous', help='When set the program will reset state and rerun after each test', 
+    action='store_true')
+parser.add_argument(
+    '-v',  '--verbose', help='Provides additional information during runtime', 
+    action='store_true')
+args = parser.parse_args()
 
 
 # Set global var for transfer data
@@ -27,17 +53,23 @@ response_li = []
 # Quality-of-life
 getSize   = lambda unit, multiplier : 1024 ** {'b': 0, 'kib': 1, 'mib': 2, 'gib': 3}[unit] * int(multiplier)
 parseData = lambda data : data.decode('utf-8').strip("'").split(';')[1].split(':')
+vPrint    = print if args.verbose else lambda *a, **k: None
 
 
-# Check to see if the system has sufficient memory (Windows)
-def checkMemWin(unit, multiplier, cycles):
+# Check to see if the system has sufficient memory
+def checkMem(unit, multiplier, cycles):
     req = getSize(unit, multiplier) * int(cycles)
 
-    avail_mem = check_output('wmic OS get FreePhysicalMemory').decode('utf-8')
-    avail_mem = ''.join(ch for ch in avail_mem if ch.isdigit()) # In KiB
-    avail_mem = int(avail_mem) * 1024 # Convert to B
+    if platform == 'linux' or platform == 'linux2':
+        _, _, avail_mem = map(int, popen('free -t -m').readlines()[-1].split()[1:]) # In MiB
+        avail_mem = int(avail_mem) * 1024 * 1024 # Convert to B
 
-    if avail_mem > req + req*(mem_buffer/100):
+    elif platform == 'win32':
+        avail_mem = check_output('wmic OS get FreePhysicalMemory').decode('utf-8')
+        avail_mem = ''.join(ch for ch in avail_mem if ch.isdigit()) # In KiB
+        avail_mem = int(avail_mem) * 1024 # Convert to B
+
+    if avail_mem > req + req*(args.membuffer/100):
         return True
 
 
@@ -45,11 +77,13 @@ def checkMemWin(unit, multiplier, cycles):
 def main():
     global response_li
 
+    vPrint(f'\nHost: Running on {args.hostip}:{args.port}...')
+
     while 1:
         # Build socket & wait
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        s.bind((host, port))
+        s.bind((args.hostip, args.port))
         s.listen(1)
 
         try:
@@ -59,15 +93,15 @@ def main():
 
             # Handle for init request, update user
             if data[0] == '0':
-                print(f'\nHost: Received new test request from [{addr[0]}] for [{data[1]}] cycles of [{data[2]} {data[3]}]')
+                vPrint(f'Host: Received new test request from [{addr[0]}] for [{data[1]}] cycles of [{data[2]} {data[3]}]')
 
                 # Empty possible remainders in-case a test was interrupted, update user
                 response_li = []
-                print('Host: Memory self-test ', end='')
+                vPrint('Host: Memory self-test ', end='')
 
                 # Self-test memory capacity
-                if checkMemWin(data[3], data[2], data[1]):
-                    print('[PASSED]\n...')
+                if checkMem(data[3], data[2], data[1]):
+                    vPrint('[PASSED]\n...')
 
                     # Generate a list of byte-literals of requested size & reply sys status good
                     response_li = [urandom(getSize(data[3], data[2])) for _ in range(int(data[1]))]
@@ -75,19 +109,23 @@ def main():
 
                 # Reply sys status bad
                 else:
-                    print('[FAILED]')
+                    vPrint('[FAILED]')
                     conn.sendall(b'00000000000000;self_test_fail')
 
             # If not initial request, send test data
             else:
-                print(f'Host: Replying to request [{data[0]}/{data[1]}]')
+                vPrint(f'Host: Replying to request [{data[0]}/{data[1]}]')
                 conn.sendall(response_li.pop())
-                print(f'      [{len(response_li)}] items remaining in memory...')
+                vPrint(f'      [{len(response_li)}] items remaining in memory...')
 
         # In-case our socket fails
-        except Exception as e:
-            #print(type(e), e)
+        except socket.error as e:
+            vPrint(f'Host: Failed... {e}')
             conn.close()
+
+        # End if continuous arg isn't set
+        if not response_li and not args.continuous:
+            break
 
 
 if __name__ == '__main__':
